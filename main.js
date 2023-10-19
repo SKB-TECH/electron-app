4535eb744c0e7d0f10820fdef151e7d745c67580
Benjamin kinyamba <shakokinyamba201@gmail.com>
2023-10-20 00:05:15 +0200

    add gitignore

---
+++ main.js
@@ -0,0 +1,23 @@
+ const {app, BrowserWindow}=require('electron');
+ const url=require('url');
+ const path=require('path')
+
+//  la function pour cree la fenetre
+
+const windowMain=()=>{
+    const win=new BrowserWindow({
+        title:"Electron App",
+        width:1000,
+        height:600
+    });
+
+
+    const urlLink=url.format({
+        pathname:path.join(__dirname,'index.html'),
+        protocol:'file'
+    })
+
+win.loadURL(urlLink);
+}
+
+app.whenReady().then(windowMain)
